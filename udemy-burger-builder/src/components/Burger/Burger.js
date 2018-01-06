9c574a8bdb12fcbb43f51f83c5c2ac4de0a2cd6d
Sivasankar D <sivasankar.d@sap.com>
2018-01-06 23:10:40 +0530

    Added state to BurgerBuilder
    Summary:
    
    Now the burger is being created via state info from BurgerBuilder

--- udemy-burger-builder/src/components/Burger/Burger.js
+++ udemy-burger-builder/src/components/Burger/Burger.js
@@ -4,11 +4,22 @@ import classes from './Burger.css';
 import BurgerIngredient from '../BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+    let ingredients = Object.keys(props.ingredients)
+                            .reduce((acc, igKey) => {
+                                let times = props.ingredients[igKey];
+                                var items = Array(times).fill(igKey).map((_, index) => {
+                                    return <BurgerIngredient key={igKey + index} type={igKey}/>
+                                });
+                                return [...acc, ...items];
+                            }, []);
+
+    if(ingredients.length === 0) {
+        ingredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+            {ingredients}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     );
--- udemy-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ udemy-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 1
+        }
+    };
+
     render() {
         return (
             <div>
-                <p>Burger Builder</p>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Burger Controls</div>
             </div>
         )
